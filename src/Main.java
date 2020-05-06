43f020ced80d839168c5e6596f3e83ef25c6f0da
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-05-05 20:04:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        directoryInformation("C:\\app\\apache-maven-3.6.3-bin");
+    }
+
+    private static void directoryInformation(String uri) throws IOException {
+        Path path = Paths.get(uri);
+
+
+        if (Files.exists(path)) {
+            System.out.println("============= Target Directory =============");
+            System.out.println("FileName : " + path.getFileName());
+            System.out.println("FileSystem : " + path.getFileSystem());
+            System.out.println("Parent : " + path.getParent());
+            System.out.println("Root : " + path.getRoot());
+            System.out.println("Absolute : " + path.isAbsolute());
+
+            System.out.println("isDirectory : " + Files.isDirectory(path));
+            System.out.println("isExecutable : " + Files.isExecutable(path));
+            System.out.println("isHidden : " + Files.isHidden(path));
+            System.out.println("isReadable : " + Files.isReadable(path));
+            System.out.println("isSymbolicLink : " + Files.isSymbolicLink(path));
+
+            if (Files.isDirectory(path)) {
+                System.out.println("============= Sub-Directories =============");
+                printFilesRecursively(path, 0);
+            }
+        }
+    }
+
+    private static void printFilesRecursively(Path path, int level) throws IOException {
+        printIndentation(level);
+        printFileName(path.getFileName().toString());
+
+        if (Files.isDirectory(path)) {
+            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path);
+            for (Path subPath : directoryStream) {
+                printFilesRecursively(subPath, level + 1);
+            }
+        }
+    }
+
+    private static void printIndentation(int indent) {
+        String dashes = "";
+        while (indent-- > 0) {
+            dashes += "-";
+        }
+        System.out.print(dashes);
+    }
+
+    private static void printFileName(String fileName) {
+        System.out.println(fileName);
+    }
+}
